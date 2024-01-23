3e4f87de2b69dabdd6d9289bde074a02a46ce402
lamahewageDulani <dulaniruwanthika99@gmail.com>
2024-01-23 17:58:24 +0530

    ngFor usage for sidebar items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-site';
+
+navItemsArray: {path: string, label: string, icon: string}[] = [
+  {path: '/', label: 'Home', icon: 'home'},
+  {path: '/contacts', label: 'Contacts', icon: 'contacts_product'},
+  {path: '/newcontact', label: 'New Contact', icon: 'person_add'}
+];
 }
