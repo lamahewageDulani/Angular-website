4c575540e79bd17d2ae4589a487d98784e168211
lamahewageDulani <dulaniruwanthika99@gmail.com>
2024-01-23 17:29:11 +0530

    dependancy changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 
+
 @NgModule({
   declarations: [
     AppComponent,
